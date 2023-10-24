343c410600069774c082fd0dcc2c061157b9b96c
Andrey <yuryev@yandex.ru>
2023-10-25 00:21:14 +0300

    * Video class

--- src/video.py
+++ src/video.py
@@ -14,10 +14,16 @@ class Video:
             part='snippet,statistics,contentDetails,topicDetails',
             id=id_video
         ).execute()
-        self.title = video_response['items'][0]['snippet']['title']
-        self.count_views = video_response['items'][0]['statistics']['viewCount']
-        self.count_likes = video_response['items'][0]['statistics']['likeCount']
-        self.comments = video_response['items'][0]['statistics']['commentCount']
+        try:
+            self.title = video_response['items'][0]['snippet']['title']
+            self.count_views = video_response['items'][0]['statistics']['viewCount']
+            self.like_count = video_response['items'][0]['statistics']['likeCount']
+            self.comments = video_response['items'][0]['statistics']['commentCount']
+        except IndexError:
+            self.title = None
+            self.count_views = None
+            self.like_count = None
+            self.comments = None
 
     def __str__(self):
         ''' Название видео '''
@@ -26,6 +32,7 @@ class Video:
 
 class PLVideo(Video):
     ''' Объект видео из плейлиста'''
+
     def __init__(self, id_video, id_pl):
         ''' получить плейлист и создать из него объект видео по id '''
         if Video.youtube is None:
